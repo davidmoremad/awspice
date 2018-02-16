b664751b8987ec82c51310070181e4d8036d1f3d
David Amrani Hernández <david.amrani@11paths.com>
2018-02-16 10:26:39 +0100

    Get exposed buckets in S3

--- awspice/services/s3.py
+++ awspice/services/s3.py
@@ -5,12 +5,45 @@ class S3Service(AwsBase):
 
     def get_buckets(self):
         '''
-        Get all buckets in S3 for a region
+        Get all buckets in S3
 
         Returns:
-            List of arrays with all buckets
+            Buckets (list): List of dictionaries with the buckets requested
         '''
         return self.inject_client_vars(self.client.list_buckets()['Buckets'])
+
+    def get_public_buckets(self):
+        '''
+        Get all public readable or writable buckets in S3
+
+        Returns:
+            Buckets-ACL (list): List of dictionaries with the buckets requested
+        '''
+        results = []
+        for bucket in self.get_buckets():
+            # Lack of permissions
+            try:
+                bucket_acl_response = self.client.get_bucket_acl(Bucket=bucket['Name'])
+
+                public_bucket = {}
+                permissions_to_check = ['READ', 'WRITE']
+                public_acl_indicator = 'http://acs.amazonaws.com/groups/global/AllUsers' # https://docs.aws.amazon.com/AmazonS3/latest/dev/acl-overview.html
+
+                for grant in bucket_acl_response['Grants']:
+                    for (k, v) in grant.iteritems():
+                        if k == 'Permission' and any(permission in v for permission in permissions_to_check):
+                            for (grantee_attrib_k, grantee_attrib_v) in grant['Grantee'].iteritems():
+                                if 'URI' in grantee_attrib_k and grant['Grantee']['URI'] == public_acl_indicator:
+                                    if bucket['Name'] in public_bucket.keys():
+                                        public_bucket[bucket['Name']].append(v)
+                                    else:
+                                        public_bucket[bucket['Name']] = []
+                                        public_bucket[bucket['Name']].append(v)
+
+                if public_bucket: results.append(public_bucket)
+            except:
+                pass
+        return self.inject_client_vars(results)
 
     def __init__(self):
         AwsBase.__init__(self, 's3')
