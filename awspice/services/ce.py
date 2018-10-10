b4d54f76624891df536417520a3ac4454b21341e
David Amrani <david.amrani@11paths.com>
2018-10-10 14:35:04 +0100

    CostExplorer get_cost - Tag names not required to get total account cost

--- awspice/services/ce.py
+++ awspice/services/ce.py
@@ -7,7 +7,7 @@ class CostExplorerService(AwsBase):
     Class belonging to the Cost Explorer service.
     '''
 
-    def get_cost(self, tag_names, from_date=None, to_date=None, interval="Monthly"):
+    def get_cost(self, from_date=None, to_date=None, tag_names=None, interval="Monthly"):
         '''
         Get the cost of item/s by its tag "Name".
 
@@ -16,23 +16,24 @@ class CostExplorerService(AwsBase):
         If the date is not indicated, the cost of the last month will be returned.
 
         Args:
-            tagnames (lst): List of tag names of machines to obtain data (wildcards are not valid)
             from_date (str): Date from which you want to obtain data. (Format: 2018-04-24)
             to_date (str): Date until which you want to obtain data. (Format: 2018-04-24)
-            interval (str):Time interval to be analyzed. [MONTHLY|DAILY]
+            tagnames (lst): List of tag names of elements to obtain data (wildcards are not valid)
+            interval (str): Time interval to be analyzed. [ MONTHLY | DAILY ]
 
         Examples:
             get_cost(['machine-1', 'machine-2'], '2018-12-24', '2018-12-26', interval='daily')
+            get_cost() # Get account cost
 
         Returns:
             Costs (list): List of days or months with the requested costs
         '''
 
-        # If the dates are empty, data from the last month will be extracted.
+        # If the dates are empty, extract data from the last month.
         if from_date is None and to_date is None:
             from_date = (datetime.datetime.now() -
-                datetime.timedelta(1*365/12)).strftime('%Y-%m-%d')
-            to_date = datetime.datetime.now().strftime('%Y-%m-%d')
+                datetime.timedelta(1*365/12)).replace(day=1).strftime('%Y-%m-%d')
+            to_date = datetime.datetime.now().replace(day=1).strftime('%Y-%m-%d')
 
         # Parameters validation
         try:
@@ -44,12 +45,17 @@ class CostExplorerService(AwsBase):
             raise ValueError("Invalid parameters")
 
         timeperiod = {'Start': from_date , 'End': to_date}
-        filters = {"Tags": {"Key": "Name", "Values": tag_names}}
+        filters = {"Tags": {"Key": "Name", "Values": tag_names}} if tag_names else None
 
-        results = self.client.get_cost_and_usage(TimePeriod=timeperiod,
-                                                 Granularity=interval.upper(),
-                                                 Filter=filters,
-                                                 Metrics=['UnblendedCost'])
+        if filters:
+            results = self.client.get_cost_and_usage(TimePeriod=timeperiod,
+                                                    Granularity=interval.upper(),
+                                                    Filter=filters,
+                                                    Metrics=['UnblendedCost'])
+        else:
+            results = self.client.get_cost_and_usage(TimePeriod=timeperiod,
+                                                    Granularity=interval.upper(),
+                                                    Metrics=['UnblendedCost'])
         return results['ResultsByTime']
 
 
